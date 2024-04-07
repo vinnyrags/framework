5e2bb0de9570a626742b1f5f979bdf27c069a229
vinnyrags <vincentpasqualeragosta@gmail.com>
2024-04-07 00:23:55 -0400

    update composer and init script

--- init.sh
+++ init.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Run composer update
-composer update
+# Run composer update without interaction
+composer update --no-interaction
 
 # Remove "vinnyrags/child-theme" from composer.json
-composer remove vinnyrags/child-theme
+sed -i '' '/"vinnyrags\/child-theme"/d' composer.json
 
 # Remove /wp-content/themes/child-theme/.git
 rm -rf wp-content/themes/child-theme/.git
 
 # Prompt the user for a project name
-read -p "Enter project name (no capital letters, use hyphens instead of spaces): " projectName
+read -p "Enter project name: " projectName
 
 # Change "name" in composer.json
-sed -i "s/\"name\": \"vincentragosta\/project\"/\"name\": \"vincentragosta\/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')\"/" composer.json
+sed -i '' "s/\"name\": \"vinnyrags\/project\"/\"name\": \"vinnyrags\/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')\"/" composer.json
 
 # Rename themes/child-theme directory
 mv wp-content/themes/child-theme wp-content/themes/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
 
 # Update themes/{input value}/style.scss Theme Name
-sed -i "s/Theme Name: Project/Theme Name: $(echo "$projectName" | sed 's/-/ /g' | sed 's/\b\(.\)/\u\1/g')/" wp-content/themes/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')/style.scss
+sed -i '' "s/Theme Name: Project/Theme Name: $(echo "$projectName" | sed 's/-/ /g' | sed 's/\b\(.\)/\u\1/g')/" wp-content/themes/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')/style.scss
 
 # Change Theme URI
-sed -i "s/Theme URI: https:\/\/project.io/Theme URI: https:\/\/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-').io/" wp-content/themes/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')/style.scss
+sed -i '' "s/Theme URI: https:\/\/project.io/Theme URI: https:\/\/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-').io/" wp-content/themes/$(echo "$projectName" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')/style.scss
 
 # Prompt the user to enter the new project git url
 read -p "Enter the new project git URL: " gitUrl
